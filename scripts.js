c6fbb9715f8d5649c196365a273623e275fb26f3
nikatandem <nikatandem@gmail.com>
2024-04-11 10:57:30 +0200

    boton musica

--- scripts.js
+++ scripts.js
@@ -1,18 +1,23 @@
 var audio = document.getElementById('audioPlayer');
-var muteButton = document.getElementById('muteButton');
-var muteIcon = document.getElementById('muteIcon');
+    var muteButton = document.getElementById('muteButton');
+    var muteIcon = document.getElementById('muteIcon');
 
-muteButton.addEventListener('click', function() {
-  if (audio.muted) {
-    audio.muted = false;
-    muteIcon.classList.remove('bi-volume-mute-fill');
-    muteIcon.classList.add('bi-volume-up-fill');
-  } else {
-    audio.muted = true;
-    muteIcon.classList.remove('bi-volume-up-fill');
-    muteIcon.classList.add('bi-volume-mute-fill');
-  }
-});
+    muteButton.addEventListener('click', function() {
+      if (audio.muted) {
+        audio.muted = false;
+        muteIcon.classList.remove('bi-volume-mute-fill');
+        muteIcon.classList.add('bi-volume-up-fill');
+      } else {
+        audio.muted = true;
+        muteIcon.classList.remove('bi-volume-up-fill');
+        muteIcon.classList.add('bi-volume-mute-fill');
+      }
+    });
+
+    function playMusic() {
+      audio.play();
+    }
+
 // Función para manejar la descarga del mapa
 function descargarMapa() {
   // Realiza cualquier operación adicional necesaria antes de la descarga
