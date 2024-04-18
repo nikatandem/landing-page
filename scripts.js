f6f4dd4508d38689a16c5b99fe5765985bba41d7
nikatandem <nikatandem@gmail.com>
2024-04-18 10:55:03 +0200

    boton top

--- scripts.js
+++ scripts.js
@@ -38,3 +38,23 @@ document.getElementById("descargarMapa").addEventListener("click", descargarMapa
 
 document.getElementById("playButton").classList.add("infinite-animation");
 document.getElementById("descargarMapa").classList.add("infinite-animation");
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
